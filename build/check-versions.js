0b196832e40078ea63db246204277b3dfb3e40f3
LyNnJeR31 <943280824@qq.com>
2018-04-08 17:26:26 +0800

    环境

--- build/check-versions.js
+++ build/check-versions.js
@@ -2,9 +2,8 @@
 const chalk = require('chalk')
 const semver = require('semver')
 const packageConfig = require('../package.json')
-const shell = require('shelljs')
 
-function exec (cmd) {
+const exec = function (cmd) {
   return require('child_process').execSync(cmd).toString().trim()
 }
 
@@ -13,16 +12,13 @@ const versionRequirements = [
     name: 'node',
     currentVersion: semver.clean(process.version),
     versionRequirement: packageConfig.engines.node
-  }
-]
-
-if (shell.which('npm')) {
-  versionRequirements.push({
+  },
+  {
     name: 'npm',
     currentVersion: exec('npm --version'),
     versionRequirement: packageConfig.engines.npm
-  })
-}
+  }
+]
 
 module.exports = function () {
   const warnings = []
