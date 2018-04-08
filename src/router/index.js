d497948f9e54515e091a658f893a4c80406035a6
LyNnJeR31 <943280824@qq.com>
2018-04-08 22:02:27 +0800

    环境

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import index from '../page/index'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+const router = [{
+  path: '/',
+  component: index,
+  meta: { auth: false }
+}, {
+
+}]
+
+export default router
