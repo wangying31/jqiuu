eb9cdc342d26eed4d50dddfc6aec22a777c1de09
LyNnJeR31 <943280824@qq.com>
2018-04-25 23:43:52 +0800

    接口调用

--- src/api/index.js
+++ src/api/index.js
@@ -24,15 +24,6 @@ export default {
   localReg: data => {
     axios.post('/user/addUser', data, config())
   },
-  articleList: data => {
-    return axios.get('/article/articleList/' + data, config())
-  },
-  photoList: data => {
-    return axios.get('/album/photoList/' + data, config())
-  },
-  photoLike: data => {
-    return axios.put('/album/' + data + '/photoLike', {}, config())
-  },
   userInfo: data => {
     return axios.get('/user' + data + '/userInfo', config())
   },
@@ -47,5 +38,59 @@ export default {
   },
   tags: data => {
     return axios.get('/article/tags', config())
+  },
+  articlePage: data => {
+    return axios.get('/article/' + data + '/articlePage', config())
+  },
+  articleCollect: data => {
+    return axios.put('/article/' + data + '/articleCollect', {}, config())
+  },
+  addArticle: data => {
+    return axios.post('/article/addArticle', data, config())
+  },
+  articleList: data => {
+    return axios.get('/article/articleList/' + data, config())
+  },
+  articleUser: data => {
+    return axios.get('/article/' + data, config())
+  },
+  articleTogether: data => {
+    return axios.get('/article/' + data + '/articleTogether', config())
+  },
+  commentList: data => {
+    return axios.get('/comment/' + data + '/commentList', config())
+  },
+  commentListAll: data => {
+    return axios.get('/comment/' + data + '/commentListAll', config())
+  },
+  addComment: data => {
+    return axios.post('/comment/addComment', data, config())
+  },
+  delComment: data => {
+    return axios.delete('/comment/' + data, config())
+  },
+  editArticle: data => {
+    return axios.put('/article/' + data.aid + '/editArticle', data, config())
+  },
+  delArticle: data => {
+    return axios.delete('/article/' + data, config())
+  },
+  articleStatus: data => {
+    return axios.put('/article/' + data + '/articleStatus', {}, config())
+  },
+  addPhoto: data => {
+    return axios.post('/album/addPhoto', data, config())
+  },
+  photoList: data => {
+    return axios.get('/album/photoList/' + data, config())
+  },
+  photoLike: data => {
+    return axios.put('/album/' + data + '/photoLike', {}, config())
+  },
+  photoUser: data => {
+    return axios.get('/album/' + data + '/photoUser', config())
+  },
+  delPhoto: data => {
+    return axios.delete('/album/' + data, config())
   }
 }
